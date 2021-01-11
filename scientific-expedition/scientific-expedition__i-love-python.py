998a6895307b1066b0c6812d289b8c045d508997
woszkiel <woszkiel@smi.pl>
2021-01-11 17:08:47 +0100

    Sci Exp two missions

---
+++ scientific-expedition/scientific-expedition__i-love-python.py
@@ -0,0 +1,6 @@
+i_love_python = lambda: "I love Python!"
+
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert i_love_python() == "I love Python!"
