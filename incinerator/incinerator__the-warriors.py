0567aabe353c8e4ac530209cfbf9d7dd4abf6ad7
witekosz <witekosz@gmail.com>
2021-01-12 02:16:22 +0100

    Add Inic island

---
+++ incinerator/incinerator__the-warriors.py
@@ -0,0 +1,41 @@
+class Warrior:
+    health = 50
+    attack = 5
+
+    @property
+    def is_alive(self):
+        return self.health > 0
+
+
+class Knight(Warrior):
+    attack = 7
+
+
+def fight(unit_1: Warrior, unit_2: Warrior) -> bool:
+    while unit_1.is_alive:
+        unit_2.health -= unit_1.attack
+        if not unit_2.is_alive:
+            break
+        unit_1.health -= unit_2.attack
+    return unit_1.is_alive
+
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+
+    chuck = Warrior()
+    bruce = Warrior()
+    carl = Knight()
+    dave = Warrior()
+    mark = Warrior()
+
+    assert fight(chuck, bruce) == True
+    assert fight(dave, carl) == False
+    assert chuck.is_alive == True
+    assert bruce.is_alive == False
+    assert carl.is_alive == True
+    assert dave.is_alive == False
+    assert fight(carl, mark) == False
+    assert carl.is_alive == False
+
+    print("Coding complete? Let's try tests!")
