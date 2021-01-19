0620e72688867ec1b46d6f4608dc1132e2a97428
witekosz <witekosz@gmail.com>
2021-01-19 14:15:43 +0100

    Mine add old solutions

---
+++ mine/mine__fizz-buzz.py
@@ -0,0 +1,18 @@
+def checkio(number: int) -> str:
+    if number % 5 == 0 and number % 3 == 0:
+        return "Fizz Buzz"
+    elif number % 3 == 0:
+        return "Fizz"
+    elif number % 5 == 0:
+        return  "Buzz"
+    else:
+        return str(number)
+
+
+# These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio(15) == "Fizz Buzz", "15 is divisible by 3 and 5"
+    assert checkio(6) == "Fizz", "6 is divisible by 3"
+    assert checkio(5) == "Buzz", "5 is divisible by 5"
+    assert checkio(7) == "7", "7 is not divisible by 3 or 5"
+    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
