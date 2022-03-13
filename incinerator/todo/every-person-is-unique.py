e3a5c6598d6c3ed90eb4ba2ed9a243c8ff0e8655
witekosz <witekosz@gmail.com>
2022-03-13 15:06:44 +0100

    Every person is unique passed

--- incinerator/todo/every-person-is-unique.py
+++ incinerator/todo/every-person-is-unique.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from datetime import datetime
+import datetime
 
 
 @dataclass
@@ -16,23 +16,33 @@ class Person:
 
     def name(self):
         return f"{self.first_name} {self.last_name}"
-    
+
     def age(self):
-        print()
-        return self.age
+        current_date = datetime.date(2018, 1, 1)
+        birth_date = datetime.datetime.strptime(self.birth_date, "%d.%m.%Y").date()
+        return (current_date - birth_date).days // 365
 
     def work(self):
-        return f"Is a {self.job}"
+        match self.gender:
+            case "male":
+                return f"He is a {self.job}"
+            case "female":
+                return f"She is a {self.job}"
+            case _:
+                return f"Is a {self.job}"
 
     def money(self):
-        return f"{self.working_years * 12 * self.salary}"
+        return f"{self.working_years * 12 * self.salary:,d}".replace(",", " ")
+
+    def home(self):
+        return f"Lives in {self.city}, {self.country}"
 
 
+# fmt: off
 if __name__ == '__main__':
     #These "asserts" using only for self-checking and not necessary for auto-testing
 
     p1 = Person("John", "Smith", "19.09.1979", "welder", 15, 3600, "Canada", "Vancouver", "male")
-    print(p1)
     p2 = Person("Hanna Rose", "May", "05.12.1995", "designer", 2.2, 2150, "Austria", "Vienna")
     assert p1.name() == "John Smith", "Name"
     assert p1.age() == 38, "Age"
